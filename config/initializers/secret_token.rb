6e909a8088bfc11eb9fba2e40a74d0ddae1a2af5
John Clem <john@minddiaper.com>
2012-10-10 12:42:34 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Binarynebula::Application.config.secret_token = 'a911dbc1f4676261a640e75aff0f6311f7d32d7118c11645b7bd69d338c944491d87c81586f22a65adce09c105b3a13477aca9d55fa4abcfc1fa35840e16430e'
